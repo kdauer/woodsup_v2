9f0bf73183e6996680933cc1487fbb8d244c2b2a
Konstantin <dauer.konstantin@gmail.com>
2020-12-31 17:12:42 +0100

    responsive navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c3b2d176ae1ea17ab168";
+/******/ 	var hotCurrentHash = "5103187a064b286755bd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/navbar.js
+++ components/navbar.js
@@ -1,17 +1,25 @@
 import Link from 'next/link'
 import { withTranslation } from '../i18n'
 
-import styles from '../styles/navbar.module.sass'
+import styles from '../styles/navbar.module.css'
 
 const Navbar = ({t}) => {
     return (
         <div className={styles.nav}>
-      <div className={styles.navLinks}>
-        <Link href="/about">{t('about')}</Link>
-        <Link href="/projects">{t('projects')}</Link>
-        <Link href="/support">{t('support')}</Link>
-        <Link href="/links">{t('motivation')}</Link>
-      </div>
+          <input type="checkbox" id={styles.navCheck} />
+          <div className={styles.navBtn}>
+            <label htmlFor={styles.navCheck}>
+            <span className={styles.line1}></span>
+            <span className={styles.line2}></span>
+            <span className={styles.line3}></span>
+            </label>
+          </div>
+        <div className={styles.navLinks}>
+          <a href="/about">{t('about')}</a>
+          <a href="/projects">{t('projects')}</a>
+          <a href="/support">{t('support')}</a>
+          <a href="/links">{t('motivation')}</a>
+        </div>
     </div>
     )
 }
