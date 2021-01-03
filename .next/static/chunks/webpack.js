e097a384253d26962f02c74fcab813cf493c1e00
Konstantin <dauer.konstantin@gmail.com>
2021-01-03 18:23:32 +0100

    contact, imprint and data protection page added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5103187a064b286755bd";
+/******/ 	var hotCurrentHash = "c0aaabdbe78673432a13";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ pages/contact.js
@@ -0,0 +1,17 @@
+const Contact = () => {
+    return (
+        <div>
+      <h3>WoodsUp e.V.</h3>
+      <p>
+        Wollestraße 52
+        <br />
+        14482 Potsdam
+      </p>
+      <p>
+        Mail: <a href="woodsup@posteo.de">woodsup@posteo.de</a>
+      </p>
+    </div>
+    )
+}
+
+export default Contact
