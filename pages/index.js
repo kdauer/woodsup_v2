1a0029e89aef67741a43ff824882b57b93506203
Konstantin <dauer.konstantin@gmail.com>
2021-01-16 22:14:46 +0100

    cookie consent

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import { withTranslation } from '../i18n'
+import CookieConsent, { Cookies } from "react-cookie-consent";
 import styles from '../styles/home.module.sass'
 import News from '../components/news'
 
@@ -9,6 +10,12 @@ const Homepage = ({t}) => (
       <div className={styles.welcome}>
       <News/>
 </div>
+<CookieConsent location="bottom"
+  buttonText={t('cookieButton')}
+  cookieName="myAwesomeCookieName2"
+          style={{ background: '#2B373B' }}
+          buttonStyle={{ color: '#4e503b', fontSize: '13px' }}
+          expires={150}>{t('cookieText')}</CookieConsent>
     </main>
   </>
 )
